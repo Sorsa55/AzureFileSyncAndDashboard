ec5ac69f3c4b2a80b4310646e8617939fed61031
Sorsa <67288622+Sorsa55@users.noreply.github.com>
2025-12-09 14:18:36 +0200

    Update sync.ps1

--- scripts/sync.ps1
+++ scripts/sync.ps1
@@ -1,4 +1,4 @@
-$sourcePath = "C:\SOURCE_FOLDER"
+$sourcePath = "ROOT:\SOURCE_FOLDER"
 $dest = "https://STORAGE_ACCOUNT.file.core.windows.net/FILE_SHARE_NAME?SAS_KEY" #This can be put in .env too. sas key can be generated in Azure Storage account-> Security Networking ->Shared access signature
 
 azcopy sync $sourcePath $dest --recursive=true
